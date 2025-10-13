7b7687f97d205486847f426e34860e2b24bba634
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-14 01:32:04 +0530

    implemented post task for user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,10 @@ import userRouter from "./routers/user.js";
 import workerRouter from "./routers/worker.js"
 
 const app = express();
+app.use(express.json());
 
 app.use("/v1/user", userRouter);
 app.use("/v1/worker", workerRouter);
-
 
 app.listen(3000, () => {
     console.log("Server runing at port: 3000");
--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -6,8 +6,11 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { authMiddlerware } from "../middleware.js";
+import { createTaskInput } from "../types.js";
 
 dotenv.config();
+
+const DEFAULT_TITLE = "select the most clickable thumbnail"
 const router = Router();
 const jwtSecret = process.env.JWT_SECRET || "";
 const prismaClient = new PrismaClient();
@@ -19,7 +22,44 @@ const s3Client = new S3Client({
     region: "ap-south-1"
 })
 
+router.post("/task", authMiddlerware, async (req, res) => {
+    //validate the input from user
+    const body = req.body;
+    // @ts-ignore
+    const userId = req.userId;
+    const parseData = createTaskInput.safeParse(body)
 
+    
+    if (!parseData.success) {
+        return res.status(411).json({
+            message: "You have sent the wrong inputs"
+        })
+    }
+
+    let response = await prismaClient.$transaction(async tx => {
+        const response = await tx.task.create({
+            data: {
+                title: parseData.data.title || "",
+                amount: "1",
+                signature: parseData.data.signature,
+                user_id: userId
+            }
+        })
+
+        await tx.option.createMany({
+            data: parseData.data.options.map(x => ({
+                image_url: x.imageUrl,
+                task_id: response.id
+            }))
+        })
+        return response
+    })
+
+    res.json({
+        id: response.id
+    })
+
+})
 
 router.get("/presignedUrl", authMiddlerware, async (req, res) => {
 
