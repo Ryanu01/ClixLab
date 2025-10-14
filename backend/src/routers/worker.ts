a25de459c26fe1c30972bb823f05581a7a575418
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-14 23:51:51 +0530

    implemented nextTask endpoint for the worker

--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -1,10 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import jwt from "jsonwebtoken";
+import { workerauthMiddlerware } from "../middleware.js";
 
 const jwtSecret = process.env.JWT_SECRET_WORKER || "";
 const prismaClient = new PrismaClient();
 const router = Router();
+
+router.get("/nextTask", workerauthMiddlerware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+
+    const task = await prismaClient.task.findFirst({
+        where: {
+            done: false,
+            submissions: {
+                none: {
+                    worker_id: userId
+                }
+            }
+        },
+        select: {
+            title: true,
+            options: true
+        }
+    })
+
+    if(!task) {
+        res.status(411).json({
+            message: "No more tasks left for you to review"
+        })
+    }else {
+        res.status(200).json({
+            task
+        })   
+    }
+})
 
 router.post("/signin", async (req, res) => {
     const hardcodedWalletAddress = process.env.PUBLIC_ADDRESS || "";
