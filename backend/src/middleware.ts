b1df2c0ffd53b4fea9269767f625022e7f57f0d2
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-14 22:14:10 +0530

    implemented worker sigin endpoint

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -23,3 +23,24 @@ export function authMiddlerware(req: Request, res: Response, next: NextFunction)
         })
     }
 }
+export function workerauthMiddlerware(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers["authorization"] ?? "";
+
+    try {
+        const decoded = jwt.verify(authHeader, process.env.JWT_SECRET_WORKER ?? "")
+        // @ts-ignore
+        if(decoded.userId) {
+            // @ts-ignore
+            req.userId = decoded.userId;
+            return next();
+        } else {
+            return res.status(403).json({
+            message: "You are not loged in"
+        })
+        }
+    } catch (error) {
+        return res.status(403).json({
+            message: "You are not loged in"
+        })
+    }
+}
--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -1,9 +1,40 @@
+import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
+const jwtSecret = process.env.JWT_SECRET_WORKER || "";
+const prismaClient = new PrismaClient();
 const router = Router();
 
-router.post("/signin", (req, res) => {
+router.post("/signin", async (req, res) => {
+    const hardcodedWalletAddress = process.env.PUBLIC_ADDRESS || "";
 
+    const existingUser = await prismaClient.worker.findFirst({
+        where: {
+            address: hardcodedWalletAddress
+        }
+    })
+
+    if (existingUser) {
+
+        const token = jwt.sign({
+            userId: existingUser.id
+        }, jwtSecret)
+        res.json({ token })
+    } else {
+
+        const user = await prismaClient.worker.create({
+            data: {
+                address: hardcodedWalletAddress,
+                pending_amount: 0,
+                locked_amount: 0
+            }
+        })
+        const token = jwt.sign({
+            userId: user.id
+        }, jwtSecret)
+        res.json({ token })
+    }
 });
 
 export default router;
