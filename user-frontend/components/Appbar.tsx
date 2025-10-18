26709472532845e2d6e808a33841cd390829af3c
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-18 15:46:22 +0530

    implemented connect wallet button for user

--- user-frontend/components/Appbar.tsx
+++ user-frontend/components/Appbar.tsx
@@ -1,11 +1,30 @@
-export const Appbar = () => { 
+import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui"
+import { useWallet } from '@solana/wallet-adapter-react';
+import { useEffect } from "react";
+
+export const Appbar = () => {
+    const { publicKey, signMessage } = useWallet();
+
+    const message = new TextEncoder().encode("Sign in to ClixLab");
+
+    async function signAndSend() {
+        const signature = await signMessage?.(message)
+        console.log(signature);
+        
+    }
+
+    useEffect(() => {
+        signAndSend()
+    })
+
     return (
         <div className="flex justify-between border-b pb-2 pt-2">
             <div className="text-2xl pl-4 flex justify-center pt-3">
-            ClixLab
+                ClixLab
             </div>
-            <div className="text-xl pr-4 pt-4">
-                Connect Wallet
+            <div className="text-xl pr-4 ">
+                {publicKey ? <WalletDisconnectButton /> : <WalletMultiButton />}
+
             </div>
         </div>
     )
